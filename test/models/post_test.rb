2c5e7afbbc8a1e9e03e05628d5c65f11b5e6a968
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-05-03 22:23:39 -0300

    added rubocop gem and add config

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,34 +1,34 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  test 'valid?' do
+  test "valid?" do
     post = posts(:one)
     assert_equal true, post.valid?
   end
 
-  test 'invalid?' do
+  test "invalid?" do
     post = posts(:invalid)
     assert_equal false, post.valid?
   end
 
-  test 'title kind_of?(String)' do
+  test "title kind_of?(String)" do
     post = posts(:one)
     assert_kind_of String, post.title
   end
 
-  test 'body kind_of?(String)' do
+  test "body kind_of?(String)" do
     post = posts(:one)
     assert_kind_of String, post.body
   end
 
-  test 'title present?' do
+  test "title present?" do
     post = posts(:two)
     assert_equal true, post.title.present?
   end
 
-  test 'body present?' do
+  test "body present?" do
     post = posts(:two)
     assert_equal true, post.body.present?
   end
